cd274e6ff53be95b6184c3dba4c10969e3a52cbb
sart55 <105958020+sart55@users.noreply.github.com>
2026-03-06 23:12:26 +0530

    Create products.jsx

---
+++ src/contexts/products.jsx
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id:1,
+    name:"Carrot",
+    price:56,
+    image:"https://i.imgur.com/3XJ2Z9R.png"
+  },
+  {
+    id:2,
+    name:"Tomato",
+    price:16,
+    image:"https://i.imgur.com/n2zK5YV.png"
+  },
+  {
+    id:3,
+    name:"Beans",
+    price:82,
+    image:"https://i.imgur.com/5bKXbR6.png"
+  },
+  {
+    id:4,
+    name:"Brinjal",
+    price:35,
+    image:"https://i.imgur.com/0Yk4W7S.png"
+  }
+];
+
+export default products;
